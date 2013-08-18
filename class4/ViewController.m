f54e171e06ee4378c2f819bf8e4143d2441b4b77
jhihguan <imdiedie@gmail.com>
2013-08-18 21:04:09 +0800

    cs193p class4 example code

--- class4/ViewController.m
+++ class4/ViewController.m
@@ -9,15 +9,72 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UILabel *selectedWordLabel;
+@property (weak, nonatomic) IBOutlet UIStepper *selectedWordStepper;
 @end
 
 @implementation ViewController
+
+- (void)addLabelAttributs:(NSDictionary *)attributes range:(NSRange)range {
+    if (range.location != NSNotFound) {
+        NSMutableAttributedString *mat = [self.label.attributedText mutableCopy];
+        [mat addAttributes:attributes range:range];
+        self.label.attributedText = mat;
+    }
+}
+
+- (void)addSelectedWordAttributes:(NSDictionary *)attributes{
+    NSRange range = [[self.label.attributedText string] rangeOfString:[self selectedWord]];
+    [self addLabelAttributs:attributes range:range];
+}
+
+- (IBAction)underline {
+    [self addSelectedWordAttributes:@{NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle)}];
+}
+- (IBAction)ununderline {
+    [self addSelectedWordAttributes:@{NSUnderlineStyleAttributeName : @(NSUnderlineStyleNone)}];
+}
+- (IBAction)changeColor:(UIButton *)sender {
+    [self addSelectedWordAttributes:@{NSForegroundColorAttributeName: sender.backgroundColor}];
+}
+
+- (IBAction)changeFont:(UIButton *)sender {
+    
+    CGFloat fontSize = [UIFont systemFontSize];
+    NSDictionary *attributes = [self.label.attributedText attributesAtIndex:0 effectiveRange:NULL];
+    UIFont *existingFont = attributes[NSFontAttributeName];
+    if (existingFont) {
+        fontSize = existingFont.pointSize;
+    }
+    UIFont *font = [sender.titleLabel.font fontWithSize:fontSize];
+    [self addSelectedWordAttributes:@{ NSFontAttributeName: font}];
+}
+
+-(NSArray *)wordList{
+    NSArray *wordList = [[self.label.attributedText string] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if ([wordList count]) {
+        return wordList;
+    }else{
+        return @[@""];
+    }
+}
+
+-(NSString *)selectedWord{
+    return [self wordList][(int)self.selectedWordStepper.value];
+}
+
+- (IBAction)updateSelectedWord {
+    self.selectedWordStepper.maximumValue = [[self wordList] count]-1;
+    self.selectedWordLabel.text = [self selectedWord];
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self updateSelectedWord];
 }
 
 - (void)didReceiveMemoryWarning
